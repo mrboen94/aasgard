b45943dcce08ab0eb91759335c25525436286c51
Mathias Bøe <mrboen94@gmail.com>
2019-12-11 16:17:22 +0100

    Cleaning the slate

--- src/Components/Pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as HomeBody } from "./home";
-export { default as AboutBody } from "./about";
-export { default as ProjectsBody } from "./projects";
-export { default as ContactBody } from "./contact";
--- src/Components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Header } from "./Header";
-export { default as ProjectCard } from "./ProjectCard";
