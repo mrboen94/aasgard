58495f8c34f2393ad6734356e1fb93acdad536eb
Mathias <mrboen94@gmail.com>
2021-11-02 14:43:16 +0100

    bump packages and upgrade

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Mathias Bøe`,
     description: `Welcome to my web hosted CV`,
-    author: `@Mathias Bøe`
+    author: `@Mathias Bøe`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -12,15 +12,15 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     {
       resolve: `gatsby-plugin-netlify-functions`,
       options: {
         functionsSrc: `${__dirname}/src/functions`,
-        functionsOutput: `${__dirname}/functions`
-      }
+        functionsOutput: `${__dirname}/functions`,
+      },
     },
     {
       resolve: `gatsby-plugin-styled-components`,
@@ -29,7 +29,7 @@ module.exports = {
         // ssr: false
         // displayName: false,
         // minify: false
-      }
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -42,11 +42,24 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/logo.png` // This path is relative to the root of the site.
-      }
-    }
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-netlify-headers`, // make sure to put last in the array
+    {
+      resolve: "gatsby-plugin-netlify",
+      options: {
+        mergeSecurityHeaders: false,
+        headers: {
+          "/*": [
+            "Content-Security-Policy: frame-ancestors 'self' https://*.aasgard.io/",
+            "X-Frame-Options: ALLOW-FROM https://aasgard.io/",
+          ],
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ]
+  ],
 };
